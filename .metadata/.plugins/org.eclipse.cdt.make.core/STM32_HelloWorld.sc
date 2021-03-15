5a6c8e09a1f1d5d1c0cb19f9b7825f91d1370536
tips367 <tipu367.khan@hotmail.com>
2021-03-15 22:23:16 +0530

    Adding STM32_HelloWorld project files

---
+++ .metadata/.plugins/org.eclipse.cdt.make.core/STM32_HelloWorld.sc
@@ -0,0 +1,9 @@
+<?xml version="1.0" encoding="UTF-8" standalone="no"?>
+<?scdStore version="2"?><scannerInfo id="org.eclipse.cdt.make.core.discoveredScannerInfo">
+<instance id="fr.ac6.managedbuild.config.gnu.cross.exe.debug.1567674608;fr.ac6.managedbuild.config.gnu.cross.exe.debug.1567674608.;fr.ac6.managedbuild.tool.gnu.cross.c.compiler.141310593;fr.ac6.managedbuild.tool.gnu.cross.c.compiler.input.c.293570173">
+<collector id="org.eclipse.cdt.make.core.PerProjectSICollector"/>
+</instance>
+<instance id="fr.ac6.managedbuild.config.gnu.cross.exe.release.837460893;fr.ac6.managedbuild.config.gnu.cross.exe.release.837460893.;fr.ac6.managedbuild.tool.gnu.cross.c.compiler.1418579544;fr.ac6.managedbuild.tool.gnu.cross.c.compiler.input.c.1085912128">
+<collector id="org.eclipse.cdt.make.core.PerProjectSICollector"/>
+</instance>
+</scannerInfo>
---
+++ STM32_HelloWorld/Debug/Third-Party/FreeRTOS/org/Source/queue.d
@@ -0,0 +1,35 @@
+Third-Party/FreeRTOS/org/Source/queue.o: \
+ ../Third-Party/FreeRTOS/org/Source/queue.c \
+ E:/Study/Projects/Embedded/RTOS_Projects/STM32_HelloWorld/Third-Party/FreeRTOS/org/Source/include/FreeRTOS.h \
+ E:/Study/Projects/Embedded/RTOS_Projects/STM32_HelloWorld/Config/FreeRTOSConfig.h \
+ E:/Study/Projects/Embedded/RTOS_Projects/STM32_HelloWorld/Third-Party/FreeRTOS/org/Source/include/projdefs.h \
+ E:/Study/Projects/Embedded/RTOS_Projects/STM32_HelloWorld/Third-Party/FreeRTOS/org/Source/include/portable.h \
+ E:/Study/Projects/Embedded/RTOS_Projects/STM32_HelloWorld/Third-Party/FreeRTOS/org/Source/include/deprecated_definitions.h \
+ E:/Study/Projects/Embedded/RTOS_Projects/STM32_HelloWorld/Third-Party/FreeRTOS/org/Source/portable/GCC/ARM_CM4F/portmacro.h \
+ E:/Study/Projects/Embedded/RTOS_Projects/STM32_HelloWorld/Third-Party/FreeRTOS/org/Source/include/mpu_wrappers.h \
+ E:/Study/Projects/Embedded/RTOS_Projects/STM32_HelloWorld/Third-Party/FreeRTOS/org/Source/include/task.h \
+ E:/Study/Projects/Embedded/RTOS_Projects/STM32_HelloWorld/Third-Party/FreeRTOS/org/Source/include/list.h \
+ E:/Study/Projects/Embedded/RTOS_Projects/STM32_HelloWorld/Third-Party/FreeRTOS/org/Source/include/queue.h \
+ E:/Study/Projects/Embedded/RTOS_Projects/STM32_HelloWorld/Third-Party/FreeRTOS/org/Source/include/task.h
+
+E:/Study/Projects/Embedded/RTOS_Projects/STM32_HelloWorld/Third-Party/FreeRTOS/org/Source/include/FreeRTOS.h:
+
+E:/Study/Projects/Embedded/RTOS_Projects/STM32_HelloWorld/Config/FreeRTOSConfig.h:
+
+E:/Study/Projects/Embedded/RTOS_Projects/STM32_HelloWorld/Third-Party/FreeRTOS/org/Source/include/projdefs.h:
+
+E:/Study/Projects/Embedded/RTOS_Projects/STM32_HelloWorld/Third-Party/FreeRTOS/org/Source/include/portable.h:
+
+E:/Study/Projects/Embedded/RTOS_Projects/STM32_HelloWorld/Third-Party/FreeRTOS/org/Source/include/deprecated_definitions.h:
+
+E:/Study/Projects/Embedded/RTOS_Projects/STM32_HelloWorld/Third-Party/FreeRTOS/org/Source/portable/GCC/ARM_CM4F/portmacro.h:
+
+E:/Study/Projects/Embedded/RTOS_Projects/STM32_HelloWorld/Third-Party/FreeRTOS/org/Source/include/mpu_wrappers.h:
+
+E:/Study/Projects/Embedded/RTOS_Projects/STM32_HelloWorld/Third-Party/FreeRTOS/org/Source/include/task.h:
+
+E:/Study/Projects/Embedded/RTOS_Projects/STM32_HelloWorld/Third-Party/FreeRTOS/org/Source/include/list.h:
+
+E:/Study/Projects/Embedded/RTOS_Projects/STM32_HelloWorld/Third-Party/FreeRTOS/org/Source/include/queue.h:
+
+E:/Study/Projects/Embedded/RTOS_Projects/STM32_HelloWorld/Third-Party/FreeRTOS/org/Source/include/task.h:
